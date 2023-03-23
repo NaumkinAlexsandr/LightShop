a978bd095c1eadc72023524541939a76faef6193
Naumkin Alexsander <naumkin437@gmail.com>
2023-03-23 22:06:36 +0200

    first commit

--- src/data/goods.js
+++ src/data/goods.js
@@ -1,98 +1,99 @@
+import a4001lt1go from "../img/a4001lt1go.jpg";
+import a1330lt1ab from "../img/a1330lt-1ab.jpg";
+import a707031101 from "../img/707031101.jpg";
+import cl803012 from "../img/cl803012.jpg";
+import a851046703 from "../img/a851046703.jpg";
+import lsp0333 from "../img/lsp0333.jpg";
+import a82844 from "../img/a82844.jpg";
+import a2872pn1ab from "../img/a2872pn1ab.jpg";
+import a41251wa from "../img/a41251wa.jpg";
+import c190wl02w from "../img/c190wl02w.jpg";
+import a390614wl from "../img/a390614wl.jpg";
+import lsp0501 from "../img/lsp0501.jpg";
+
 export const goods = [
   {
     id: "ln01",
     category: "table",
     name: "Table lamp Arte Lamp MERGA A4001LT-1GO",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/4b/1945380/big/a4001lt1go_1.jpg",
+    poster: a4001lt1go,
     price: 25,
   },
   {
     id: "ln02",
     category: "table",
     name: "Table lamp Arte Lamp Junior A1330LT-1AB",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/ca/65200/big/a1330lt_1ab_2.jpg",
+    poster: a1330lt1ab,
     price: 38,
   },
   {
     id: "ln03",
     category: "table",
     name: "Table lamp De Markt  707031101",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/3d/2432371/big/707031101_1.jpg",
+    poster: a707031101,
     price: 46,
   },
   {
     id: "ln04",
     category: "table",
     name: "Table lamp Newton CL803012",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/70/106043/big/cl803012_12.jpg",
+    poster: cl803012,
     price: 51,
   },
   {
     id: "ln05",
     category: "table",
     name: "Table lamp SOLO 1 851046703",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/09/27580/big/85104_6.jpg",
+    poster: a851046703,
     price: 39,
   },
   {
     id: "flor01",
     category: "flor",
     name: "Floor lamp Lussole LGO LOFT LSP-0333",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/99/64747/big/lsp_0333_11.jpg",
+    poster: lsp0333,
     price: 70,
   },
   {
     id: "flor02",
     category: "flor",
     name: "Floor lamp  Odeon Light Trend 2713/F",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/ef/27601/big/82844_6.jpg",
+    poster: a82844,
     price: 45,
   },
   {
     id: "flor03",
     category: "flor",
     name: "Floor lamp Arte Lamp California A2872PN-1AB",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/35/33823/big/a2872pn_1ab_4.jpg",
+    poster: a2872pn1ab,
     price: 62,
   },
   {
     id: "flor04",
     category: "flor",
     name: "Floor lamp Nevada Z329-FL-01-CH",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/09/189497/big/189497.1.jpg",
+    poster: lsp0501,
     price: 89,
   },
   {
     id: "bra01",
     category: "bra",
     name: "Sconce Odeon Light Arta 4125/1WA",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/21/197891/big/41251wa_15.jpg",
+    poster: a41251wa,
     price: 78,
   },
   {
     id: "bra02",
     category: "bra",
     name: "Sconce Parma C190-WL-02-W",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/fe/133650/big/c190_wl_02_w_1.jpg",
+    poster: c190wl02w,
     price: 40,
   },
   {
     id: "bra03",
     category: "bra",
     name: "Sconce Mantra TORCH 67036703",
-    poster:
-      "https://cdn.basicdecor.ru/files/media/product_photos/a2/331928/big/390614wl_9.jpg",
+    poster: a390614wl,
     price: 88,
   },
 ];
